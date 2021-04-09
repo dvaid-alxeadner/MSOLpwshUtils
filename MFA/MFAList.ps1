1ca05a5f4b4c7fe7046db15e10f5f4253048dd8c
dvaid-alxeadner <59740048+dvaid-alxeadner@users.noreply.github.com>
2021-04-09 14:23:09 -0500

    Add files via upload

---
+++ MFA/MFAList.ps1
@@ -0,0 +1,13 @@
+try {
+    
+    # Connect To Office (Interactive Login)
+    Connect-MsolService -Credential $userCredential
+    # Export users to CVS File
+    Get-MsolUser -all | Select-Object @{N="Names"; E={$_.DisplayName -replace ",","-"}},Title,UserPrincipalName,BlockCredential,isLicensed,@{N="MFA Status"; E={ if( $_.StrongAuthenticationRequirements.State -ne $null){ $_.StrongAuthenticationRequirements.State} else { "Disabled"}}} | Export-csv -path "MFA.csv" -NoTypeInformation -Encoding UTF8 
+}
+Catch
+{
+    Write-Output $_.Exception.GetType().FullName, $_.Exception.Message
+    Write-Host "Error please report in https://github.com/dvaid-alxeadner/MSOLpwshUtils" 
+    exit 
+}
